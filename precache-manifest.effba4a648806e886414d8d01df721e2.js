b839da7443c06ac961a62c064ed833fb93128fc7
ApexSukhee <apexsukhee@gmail.com>
2020-12-27 13:02:21 +0845

    Updates

---
+++ precache-manifest.effba4a648806e886414d8d01df721e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594ab22d2737692a3650feba395359db",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "0aed4ebdb4ac9aa347df",
+    "url": "/hello/static/js/2.c229897c.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/hello/static/js/2.c229897c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e32d6a59952bae7aa9",
+    "url": "/hello/static/js/main.2492a519.chunk.js"
+  },
+  {
+    "revision": "69940b62a50d6122d068",
+    "url": "/hello/static/js/runtime-main.b910b0dd.js"
+  }
+]);
